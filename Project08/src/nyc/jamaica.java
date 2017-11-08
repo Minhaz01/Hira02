35fecbafd0c0e5d6a84c0f3ece8cfdfd45fc732d
Minhaz01 <33205845+Minhaz01@users.noreply.github.com>
2017-11-07 20:27:26 -0600

    Update jamaica.java
    hi

--- Project08/src/nyc/jamaica.java
+++ Project08/src/nyc/jamaica.java
@@ -4,7 +4,8 @@ public class jamaica {
 
 	public static void main(String[] args) {
 		System.out.println(" Hellow git learner ");
-
+		System.out.println(" Hellow git learner, i am from remote ripo ");
+		System.out.println(" Hellow git learner, i am from remote ripo ");
 	}
 
 }
